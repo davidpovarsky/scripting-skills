dbf387b912ba80f5d745ca2c68cfa08ee4af2073
davidpovarsky <davidpovarski1@gmail.com>
2026-08-18 11:14:09 +0300

    Check Israel Transit updates before compact queries

--- israel-transit/scripts/query.ts
+++ israel-transit/scripts/query.ts
@@ -1,7 +1,9 @@
 import { Script } from "scripting"
 import { execute } from "./lib/transit"
 import { TransitRequest } from "./lib/types"
+import { maybeAutoUpdate } from "./lib/auto-update"
 async function main(){
+  await maybeAutoUpdate()
   const req=Script.queryParameters as TransitRequest
   const result=await execute(req)
   Script.exit(result)
