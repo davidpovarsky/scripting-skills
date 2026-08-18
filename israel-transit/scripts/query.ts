ed6b101b45e756edc231ff081d2294aa8ff81fe8
davidpovarsky <davidpovarski1@gmail.com>
2026-08-18 11:18:29 +0300

    Remove redundant transit self-updater hook

--- israel-transit/scripts/query.ts
+++ israel-transit/scripts/query.ts
@@ -1,9 +1,8 @@
 import { Script } from "scripting"
 import { execute } from "./lib/transit"
 import { TransitRequest } from "./lib/types"
-import { maybeAutoUpdate } from "./lib/auto-update"
+
 async function main(){
-  await maybeAutoUpdate()
   const req=Script.queryParameters as TransitRequest
   const result=await execute(req)
   Script.exit(result)
